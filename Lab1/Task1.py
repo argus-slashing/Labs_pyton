c2336d9486d0a0795589469f4ad86c15c82ed705
argus-slashing <itsagoodluck@gmail.com>
2022-02-03 10:59:00 +0300

    task1

---
+++ Lab1/Task1.py
@@ -0,0 +1,21 @@
+def get_count(str):
+    tmp = 0
+    for i in str:
+        tmp +=1
+        if i == ".":
+            break
+    return(len(str) - tmp)
+
+
+
+def transform():
+    s = input("Введите число:")
+    cash = s.replace(",",".")
+    count = get_count(cash)
+    if float(cash) < 0.00:
+        print("Некорректный формат!")
+    else:
+        key = float(cash)%1
+        print(int(key*10**count), "копеек")
+
+transform()
